508d5e97a937038329587b0bb047f24b47498a81
adamskki <adamski.michal98@gmail.com>
2019-03-04 21:21:01 +0100

    Initial Project Add

---
+++ src/main/java/adams/springframework/petclinicproject/PetClinicProjectApplication.java
@@ -0,0 +1,13 @@
+package adams.springframework.petclinicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicProjectApplication.class, args);
+    }
+
+}
