68163eb59a5a3f1ce15813d35771b60491c47e91
adamskki <adamski.michal98@gmail.com>
2019-03-20 20:09:08 +0100

    adding base entity. Closes #10

--- pet-clinic-data/src/main/java/adams/springframework/petclinicproject/model/model/Person.java
+++ pet-clinic-data/src/main/java/adams/springframework/petclinicproject/model/model/Person.java
@@ -1,6 +1,6 @@
 package adams.springframework.petclinicproject.model.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/adams/springframework/petclinicproject/model/model/PetType.java
+++ pet-clinic-data/src/main/java/adams/springframework/petclinicproject/model/model/PetType.java
@@ -1,6 +1,6 @@
 package adams.springframework.petclinicproject.model.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
