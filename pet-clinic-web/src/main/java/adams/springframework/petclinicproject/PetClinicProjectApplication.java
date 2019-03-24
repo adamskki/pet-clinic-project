bb758dcea6edbc23503911c6e2031385d3eb56dc
adamskki <adamski.michal98@gmail.com>
2019-03-24 17:05:16 +0100

    Adding Index Page and controller, closes #8

--- pet-clinic-web/src/main/java/adams/springframework/petclinicproject/PetClinicProjectApplication.java
+++ pet-clinic-web/src/main/java/adams/springframework/petclinicproject/PetClinicProjectApplication.java
@@ -1,4 +1,4 @@
-package petclinicproject;
+package adams.springframework.petclinicproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
