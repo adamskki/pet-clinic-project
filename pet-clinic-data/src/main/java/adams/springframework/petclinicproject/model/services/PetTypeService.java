997cda61c46bea2ca199eda7e92f0c60098b4dc1
adamskki <adamski.michal98@gmail.com>
2019-04-10 00:27:49 +0200

    added PetTypeService closes #29

---
+++ pet-clinic-data/src/main/java/adams/springframework/petclinicproject/model/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package adams.springframework.petclinicproject.model.services;
+
+import adams.springframework.petclinicproject.model.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
