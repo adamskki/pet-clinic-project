aedeb986de9eb10df8dcb1ebc737365f08432dda
adamskki <adamski.michal98@gmail.com>
2019-04-10 11:47:18 +0200

    Added Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/adams/springframework/petclinicproject/model/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package adams.springframework.petclinicproject.model.services;
+
+import adams.springframework.petclinicproject.model.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
