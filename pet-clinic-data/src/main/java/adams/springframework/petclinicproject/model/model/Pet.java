00f485dc409843d705c425cc49aa287f9d67c66c
adamskki <adamski.michal98@gmail.com>
2019-04-12 14:00:47 +0200

    Added Visit Entity, closes #38

--- pet-clinic-data/src/main/java/adams/springframework/petclinicproject/model/model/Pet.java
+++ pet-clinic-data/src/main/java/adams/springframework/petclinicproject/model/model/Pet.java
@@ -2,6 +2,7 @@ package adams.springframework.petclinicproject.model.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +21,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits;
 
     public String getName() {
         return name;
@@ -51,5 +55,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
