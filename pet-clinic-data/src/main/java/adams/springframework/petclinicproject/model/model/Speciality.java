8e07bc03601ec10d44d1a97a5d275ff6c9540199
adamskki <adamski.michal98@gmail.com>
2019-04-12 11:00:44 +0200

    updated vet to use JPA. closes #37

--- pet-clinic-data/src/main/java/adams/springframework/petclinicproject/model/model/Speciality.java
+++ pet-clinic-data/src/main/java/adams/springframework/petclinicproject/model/model/Speciality.java
@@ -1,7 +1,14 @@
 package adams.springframework.petclinicproject.model.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
