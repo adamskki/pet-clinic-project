dca28727f2fc57fe00d9995bf1c639476821ec86
adamskki <adamski.michal98@gmail.com>
2019-04-12 16:32:50 +0200

    Implemented Map Impl for Visit Service

---
+++ pet-clinic-data/src/main/java/adams/springframework/petclinicproject/model/services/VisitService.java
@@ -0,0 +1,6 @@
+package adams.springframework.petclinicproject.model.services;
+
+import adams.springframework.petclinicproject.model.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
